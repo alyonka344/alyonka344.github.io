15008af8014160953d7d9a960639d959b3acf99e
alyonka344 <aaatttyyy@bk.ru>
2024-12-10 20:30:18 +0300

    lab 4

--- MyGallery/index.js
+++ MyGallery/index.js
@@ -1,1 +1,27 @@
-console.log('Happy developing ✨')
+(function () {
+    function setActiveMenuItem() {
+        const menuItems = document.querySelectorAll('.menu__link');
+        const currentPath = window.location.pathname;
+
+        menuItems.forEach(item => {
+            if (item.pathname === currentPath) {
+                item.classList.add('active');
+            }
+        });
+    }
+
+    function showLoadTime() {
+        const loadTime = window.performance.now();
+        const footer = document.querySelector('footer');
+
+        const loadTimeElement = document.createElement('p');
+        loadTimeElement.classList.add('load-time');
+        loadTimeElement.textContent = `Время загрузки страницы: ${loadTime.toFixed(2)} мс`;
+        footer.appendChild(loadTimeElement);
+    }
+
+    window.addEventListener('load', () => {
+        setActiveMenuItem();
+        showLoadTime();
+    });
+})();

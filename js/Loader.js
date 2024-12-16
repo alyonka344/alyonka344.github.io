a395cb2d7a978c5dea6419600240e9e5c6af67af
alyonka344 <aaatttyyy@bk.ru>
2024-12-17 01:02:28 +0300

    lab 7

---
+++ js/Loader.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", async () => {
+    const targetUrl = "MyGallery.html";
+
+    const response = await fetch(targetUrl);
+
+    if (!response.ok) {
+        throw new Error(`Ошибка загрузки: ${response.status}`);
+    }
+    await response.text();
+
+    setTimeout(() => {
+        window.location.href = targetUrl;
+    }, 2000);
+});
---
+++ js/Swiper.js
@@ -0,0 +1,27 @@
+﻿const swiper = new Swiper('#paintings-gallery', {
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 9,
+    freeMode: true,
+    mousewheel: true,
+    watchOverflow: true,
+});
+
+const gallery = document.querySelector('.my-paintings__images');
+
+gallery.addEventListener('wheel', (event) => {
+    const delta = event.deltaY;
+
+    const isAtStart = swiper.isBeginning && delta < 0;
+    const isAtEnd = swiper.isEnd && delta > 0;
+
+    if (!isAtStart && !isAtEnd) {
+        event.preventDefault();
+        return;
+    }
+    if (isAtEnd && delta > 0) {
+        window.scrollBy(0, 50);
+    } else if (isAtStart && delta < 0) {
+        window.scrollBy(0, -50);
+    }
+});
